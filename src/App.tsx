42bc81a92492c177d080737b3ea25ab9b22e1aeb
Felipe <91050670+FelipeFama@users.noreply.github.com>
2022-10-11 20:29:55 -0300

    add new packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
+import React from "react" 
 
-function App() {
+export default function App() {
   
   return (
    <h1 className="text-3xl font-bold underline">Hello World!</h1>
   )
 }
-
-export default App
