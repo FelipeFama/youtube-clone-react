401e2dc2ed7545906789dd76d7ee981ffa4bcfb9
Felipe <91050670+FelipeFama@users.noreply.github.com>
2022-10-11 22:00:47 -0300

    project organization

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { AiOutlineSearch, AiOutlineClose } from "react-icons/ai";
+import { TiMicrophone } from "react-icons/ti";
+import { BsYoutube, BsCameraVideo, BsBell } from "react-icons/bs";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { IoAppsSharp } from "react-icons/io5";
+
+export default function Navbar() {
+  return (
+    <div>Navbar</div>
+  )
+}
