82cbdd916dfef0d38a41c692bba0b3dfafaaf766
Felipe <91050670+FelipeFama@users.noreply.github.com>
2022-10-11 23:57:47 -0300

    navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,9 +4,50 @@ import { TiMicrophone } from "react-icons/ti";
 import { BsYoutube, BsCameraVideo, BsBell } from "react-icons/bs";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { IoAppsSharp } from "react-icons/io5";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <nav className="flex justify-between items-center px-14 h-14 bg-[#212121]">
+     <div className="flex gap-8 items-center text-2">
+        <GiHamburgerMenu />
+        <Link to="/">
+          <div className="flex gap-1 items-center justify-center">
+           <BsYoutube className="text-3xl text-red-600" />
+           <span className="text-xl font-medium">YouTube</span>
+         </div>
+        </Link>
+     </div>
+     <div className="flex items-center justify-center gap-5">
+      <form action="">
+        <div className="flex bg-zinc-900 items-center h-10 px-4 pr-0">
+          <div className="flex gap-4 items-center pr-5">
+            <AiOutlineSearch className="text-xl" />
+            <input 
+             type="text" 
+             className="w-96 bg-zinc-900 focus:outline-none border-none" 
+            />
+            <AiOutlineClose className="text-xl cursor-pointer" />
+          </div>
+          <button className="h-10 w-16 flex items-center justify-center bg-zinc-800">
+            <AiOutlineSearch className="text-xl" />
+          </button>
+        </div>
+      </form>
+      <div className="text-xl p-3 bg-zinc-900 rounded-full">
+        <TiMicrophone />
+      </div>
+     </div>
+     <div className="flex gap-5 items-center text-xl">
+      <BsCameraVideo />
+      <IoAppsSharp />
+      <div className="relative">
+        <BsBell />
+      </div>
+      <span className="absolute bottom-2 left-2 text-xs bg-red-600 rounded-full px-1">
+        9+
+      </span>
+     </div>
+   </nav>
+  );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React from "react";
+import Navbar from "../components/Navbar";
 
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+   <header>
+     <Navbar />
+   </header>
+  );
 }
